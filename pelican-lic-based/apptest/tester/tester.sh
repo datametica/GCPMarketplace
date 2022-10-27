43b03125d65510ffd894ad19b87057e15696ad37
Harshit Soni <harshit.soni@datametica.com>
2022-10-27 14:28:45 +0530

    pelican-lic-based

---
+++ pelican-lic-based/apptest/tester/tester.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -xeo pipefail
+shopt -s nullglob
+
+SERVICE=$(kubectl get service | awk '/web-service/ { print $1 }')
+
+EXTERNAL_IP=$(kubectl get service "${SERVICE}" \
+  --namespace "${NAMESPACE}" \
+  -ojsonpath="{.spec.clusterIP}")
+
+export EXTERNAL_IP
+
+for test in /tests/*; do
+  testrunner -logtostderr "--test_spec=${test}"
+done
